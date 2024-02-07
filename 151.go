6dd609fca9fba640aee994f8fecbb8640a90a068
Amirkhan <torexanovich.l@gmail.com>
2024-02-07 11:45:14 +0500

    https://leetcode.com/problems/reverse-words-in-a-string/submissions/1168496148/\?envType\=study-plan-v2\&envId\=leetcode-75

--- 151.go
+++ 151.go
@@ -1,51 +1,39 @@
 package main
 
 import (
-    "strings"
+	"strings"
 )
 
 func reverseWords(s string) string {
-    s = strings.TrimSpace(s)
-    
-    end := len(s) - 1
-    var start int
-    var reversed string
-    
-    for end >= 0 {
-        // Move end pointer to the end of the current word
-        for end >= 0 && s[end] == ' ' {
-            end--
-        }
-        
-        // Move start pointer to the beginning of the current word
-        start = end
-        for start >= 0 && s[start] != ' ' {
-            start--
-        }
-        
-        // Extract and append word to the result string
-        word := s[start+1 : end+1]
-        if len(reversed) == 0 {
-            reversed = word
-        } else {
-            reversed = reversed + " " + word
-        }
-        
-        // Move end pointer to the position just before the current word
-        end = start
-    }
-    
-    return reversed
+	s = strings.TrimSpace(s)
+	i, j, res := 0, len(s)-1, ""
+
+	for j >= 0 {
+		for j >= 0 && s[j] == ' ' { j-- }
+
+		i = j
+		for i >= 0 && s[i] != ' ' { i-- }
+
+		word := s[i+1 : j+1]
+
+		if len(res) == 0 {
+			res = word
+		} else {
+			res = res + " " + word
+		}
+
+		j = i
+	}
+
+	return res
 }
 
-func main() {
-    // Test cases
-    s1 := "the sky is blue"
-    s2 := "  hello world  "
-    s3 := "a good   example"
-    
-    // Output the reversed strings
-    println(reverseWords(s1)) // Output: "blue is sky the"
-    println(reverseWords(s2)) // Output: "world hello"
-    println(reverseWords(s3)) // Output: "example good a"
-}
+// func main() {
+// 	s1 := "the sky is blue"
+// 	s2 := "  hello world  "
+// 	s3 := "a good   example"
+
+// 	println(reverseWords(s1)) // Output: "blue is sky the"
+// 	println(reverseWords(s2)) // Output: "world hello"
+// 	println(reverseWords(s3)) // Output: "example good a"
+// }
